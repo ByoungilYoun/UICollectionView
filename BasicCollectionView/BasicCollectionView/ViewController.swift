a1dbe8e416187be259d5586ab1bb957f43dc3885
YounByoungil <jimmy7525@naver.com>
2021-08-14 21:17:52 +0900

    Setup Project

---
+++ BasicCollectionView/BasicCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCollectionView
+//
+//  Created by 윤병일 on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+
+
+}
+
