445b343ac69ba6bbb6d6e35f276c82cfbf382cb9
YounByoungil <jimmy7525@naver.com>
2021-08-14 22:31:43 +0900

    CollectionViewDelegateFlowLayout & CollectionViewDataSource

--- BasicCollectionView/BasicCollectionView/ViewController.swift
+++ BasicCollectionView/BasicCollectionView/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController {
   //MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    let layout = UICollectionViewFlowLayout()
     
-    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
     collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.dataSource = self
+    collectionView.delegate = self
+    collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     collectionView.backgroundColor = .red
     
     view.addSubview(collectionView)
@@ -23,5 +27,35 @@ class ViewController: UIViewController {
     collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
     collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+  }
+}
+
+  //MARK: - UICollectionViewDataSource
+extension ViewController : UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 4
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    cell.backgroundColor = .blue
+    let label = UILabel()
+    label.text = "hi swift"
+    label.translatesAutoresizingMaskIntoConstraints = false
+    
+    cell.contentView.addSubview(label)
+    label.topAnchor.constraint(equalTo: cell.contentView.topAnchor).isActive = true
+    label.leftAnchor.constraint(equalTo: cell.contentView.leftAnchor).isActive = true
+    label.rightAnchor.constraint(equalTo: cell.contentView.rightAnchor).isActive = true
+    label.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor).isActive = true
+ 
+    return cell
+  }
+}
+
+  //MARK: - UICollectionViewDelegateFlowLayout
+extension ViewController : UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 100, height: 100)
   }
 }
